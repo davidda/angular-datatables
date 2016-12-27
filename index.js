c2c9f132959d8a69221e80dd82ab7df1bbcfec91
davidda <davidda@users.noreply.github.com>
2016-12-27 17:18:46 +0100

    changed index.js to es5. not sure what the purpose of this file is anyways.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 /**
  * @license
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://raw.githubusercontent.com/l-lin/angular-datatables/master/LICENSE
  */
-export * from './dist/angular-datatables';
+__export(require('./dist/angular-datatables'));
+//# sourceMappingURL=index.js.map
